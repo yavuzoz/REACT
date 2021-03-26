db6d63143b44f7bf3da998d349b52e9d2372b575
yavuz <ozbayavz@gmail.com>
2021-03-26 21:32:35 +0100

    create a new profil page

---
+++ Week 03 - Create Profile Page in React (b)/newprofil/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Profil from './component/profile'
+function App() {
+  return (
+    <div className="App">
+
+      <Profil />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week 03 - Create Profile Page in React (b)/newprofil/src/component/profile.js
@@ -0,0 +1,42 @@
+export default function Profil() {
+        return (<div>
+            <header>
+                <p>Hello, I'm yavuz and 37 years old. I have been living in Switzerland for 15 months. I came here from Turkey. In Turkey chemist, chemistry teachin and I worked as a human resources specialist. I had a very good job and life. but there can be surprises in life. I had to leave my liked job and country. and I found myself in Switzerland without planning. I love Switzerland. and I got to met software and programming in this country. I got a good level in a short time. I have two online courses. Clarusway and Hicoders. I love the software and it excites me to learn new things. I like to read articles in german and english except for my software lessons. I love running, walking, cooking, drawing.</p>
+              
+                <table>
+                    <tr>
+                        <td>
+                            <b>iSİM</b>
+                        </td>
+                        <td>
+                            yavuz
+                         </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <b>Soyisim</b>
+                        </td>
+                        <td>
+                            özbay
+                         </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <b>Iletişim</b>
+                        </td>
+                        <td>
+                            ozbayavz@gmail.com
+                         </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <b>Hobbies</b>
+                        </td>
+                        <td>
+                            Futbol,Film,Kitap
+                         </td>
+                    </tr>
+                </table>
+            </header>
+        </div>)
+    }
